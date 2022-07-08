81b20e6fc99ba31d41ab71d10f3cb536723b990a
Sujit Das <to.sujitdas@gmail.com>
2022-07-08 16:35:54 +0530

    Update sort_and_search.cpp

--- sort_and_search.cpp
+++ sort_and_search.cpp
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Moonlight; if not, see <http://www.gnu.org/licenses/>.
+ * along with this software; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include<stdio.h>
